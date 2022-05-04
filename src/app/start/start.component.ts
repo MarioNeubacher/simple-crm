0ffd6622f0ebe7f844315e4154c4da8d2c1380c5
Mario Neubacher <neubacher.mario00@gmail.com>
2022-05-04 15:28:03 +0200

    registration password match with errorStateMatcher

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,49 +1,44 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FirebaseService } from '../services/firebase.service';
-import { CustomErrorStateMatcher } from './custom-state-matcher';
+import { MyErrorStateMatcher } from './MyErrorStateMatcher ';
 
 @Component({
   selector: 'app-start',
   templateUrl: './start.component.html',
   styleUrls: ['./start.component.scss']
 })
-export class StartComponent implements OnInit {
+export class StartComponent {
 
   hide1 = true;
   hide2 = true;
   hide3 = true;
 
-  /* customErrorStateMatcher = new CustomErrorStateMatcher;
-
-  signInForm: FormGroup; */
+  matcher = new MyErrorStateMatcher();
 
   signInForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required),
   });
 
- /*  signUpForm = new FormGroup({
+  checkPasswords: ValidatorFn = (group: AbstractControl):  ValidationErrors | null => { 
+    let pass = group.get('password1').value;
+    let confirmPass = group.get(signUpForm['controls']['password2']).value
+    return pass === confirmPass ? null : { notSame: true }
+  }
+
+  signUpForm = new FormGroup({
     username: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', Validators.required),
-  }); */
+    password1: new FormControl('', Validators.required),
+    password2: new FormControl('', Validators.required)
+  }, { validators: this.checkPasswords });
 
   constructor(
     public router: Router,
-    public firebaseSerice: FirebaseService/* ,
-    private fb: FormBuilder */
-  ) { 
-   /*  this.signInForm = this.fb.group({
-      email: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required]),
-    }) */
-  }
-
-  ngOnInit(): void {
-    
-  }
+    public firebaseSerice: FirebaseService
+  ) {   }
 
   openApp() {
     this.router.navigateByUrl('/home/dashboard');
@@ -57,8 +52,11 @@ export class StartComponent implements OnInit {
     return this.signInForm.get('password');
   }
 
-  enableSubmitBtn() {
+  enableSubmitBtn1() {
     return this.signInForm.valid;
   }
 
+  enableSubmitBtn2() {
+    return this.signUpForm.valid;
+  }
 }
