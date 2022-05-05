c5ca3578ffe0723e791949cfeedf22ff255887ee
Mario Neubacher <neubacher.mario00@gmail.com>
2022-05-05 11:53:51 +0200

    registration first password no error when mismatch

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { FirebaseService } from '../services/firebase.service';
-import { MyErrorStateMatcher } from './MyErrorStateMatcher ';
 
 @Component({
   selector: 'app-start',
@@ -15,25 +14,17 @@ export class StartComponent {
   hide2 = true;
   hide3 = true;
 
-  matcher = new MyErrorStateMatcher();
-
   signInForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required),
   });
-
-  checkPasswords: ValidatorFn = (group: AbstractControl):  ValidationErrors | null => { 
-    let pass = group.get('password1').value;
-    let confirmPass = group.get(signUpForm['controls']['password2']).value
-    return pass === confirmPass ? null : { notSame: true }
-  }
 
   signUpForm = new FormGroup({
     username: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
     password1: new FormControl('', Validators.required),
     password2: new FormControl('', Validators.required)
-  }, { validators: this.checkPasswords });
+  });
 
   constructor(
     public router: Router,
@@ -59,4 +50,21 @@ export class StartComponent {
   enableSubmitBtn2() {
     return this.signUpForm.valid;
   }
+
+  get password1(): AbstractControl {
+    return this.signUpForm.controls['password1'];
+  }
+  
+  get password2(): AbstractControl {
+    return this.signUpForm.controls['password2'];
+  }
+
+  onPasswordChange() {
+    if (this.password2.value == this.password1.value) {
+      this.password2.setErrors(null);
+    } else {
+      this.password2.setErrors({ mismatch: true });
+    }
+  }
+
 }
